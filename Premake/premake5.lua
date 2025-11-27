929073d4432943d8d10eef6386792f4fc21d6060
Hjalmar Danielsson <hjalmar.danielsson@learnet.se>
2025-11-27 15:30:10 +0100

    Fixing project generation: some errors in premake5.lua, and adding a bat file to generate sln and project files.

--- Premake/premake5.lua
+++ Premake/premake5.lua
@@ -1,6 +1,7 @@
 workspace("HD_CommonUtilities")
 	configurations({"Debug", "Release"})
 	platforms({"x64"})
+	location("..")
 	
 project("HD_CommonUtilities")
 	kind("StaticLib")
@@ -10,22 +11,23 @@ project("HD_CommonUtilities")
 	files("../HD_CommonUtilities/*.cpp")
 	files("../HD_CommonUtilities/HD_CommonUtilities.natvis")
 	pchheader("stdafx.h")
-	pchsource("stdafx.cpp")
+	pchsource("../HD_CommonUtilities/stdafx.cpp")
 	
 	filter({"configurations:Debug"})
 		targetname("HD_CommonUtilities_Debug")
+		targetdir("../Output/HD_CommonUtilities")
 	
 	filter({"configurations:Release"})
 		targetname("HD_CommonUtilities_Release")
-	
-	targetdir("../Output/HD_CommonUtilities")
+		targetdir("../Output/HD_CommonUtilities")
+		
 	warnings("Extra")
-	flags({"FatalWarnings"})
+	fatalwarnings({"All"})
 	vpaths
 	{
 		["Containers"] = { "HD_ArrayIterator.h", "HD_GrowingArray.h", "HD_Map.h", "HD_Pair.h", "HD_StaticArray.h", "HD_StaticStack.h", "HD_String.h" },
 		["Math"] = { "HD_Box.h", "HD_Math.h", "HD_Vector2.h" },
-		["Misc"] = { "HD_Random.h", "HD_Rancom.cpp", "Types.h", "Utilities.h" }
+		["Misc"] = { "HD_Random.h", "HD_Rancom.cpp", "Types.h", "Utilities.h" },
 		["Natvis"] = { "HD_CommonUtilities.natvis" }
 	}
 	
@@ -38,16 +40,17 @@ project("Tests")
 	libdirs({"../Output/HD_CommonUtilities"})
 	includedirs({"../HD_CommonUtilities"})
 	pchheader("stdafx.h")
-	pchsource("stdafx.cpp")
+	pchsource("../Tests/stdafx.cpp")
 	
 	filter({"configurations:Debug"})
 		links({"HD_CommonUtilities_Debug"})
 		targetname("Tests_Debug")
+		targetdir("../Output/Tests")
 	
 	filter({"configurations:Release"})
 		links({"HD_CommonUtilities_Release"})
 		targetname("Tests_Release")
+		targetdir("../Output/Tests")
 	
-	targetdir("../Output/Tests")
 	warnings("Extra")
-	flags({"FatalWarnings"})
+	fatalwarnings({"All"})
---
+++ generate_sln.bat
@@ -0,0 +1,3 @@
+mkdir Output\
+call "Premake/premake5" --file="Premake/premake5.lua" vs2022
+pause
