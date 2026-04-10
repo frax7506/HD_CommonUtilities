17acd18c1ae6e63dfc6d3ad382e595b330781ddb
Hjalmar Danielsson <hjalmar.danielsson@learnet.se>
2026-04-10 17:14:30 +0200

    Wip for the logger class

--- HD_CommonUtilities/HD_Logger.cpp
+++ HD_CommonUtilities/HD_Logger.cpp
@@ -1,4 +1,12 @@
 #include "stdafx.h"
 #include"HD_Logger.h"
 
+HD_Logger::HD_Logger()
+{
+	myLogThread = std::thread(&HD_Logger::LogThread, this);
+}
 
+HD_Logger::~HD_Logger()
+{
+	myLogThread.join();
+}
--- HD_CommonUtilities/HD_Logger.h
+++ HD_CommonUtilities/HD_Logger.h
@@ -2,11 +2,24 @@
 
 #include "HD_Singleton.h"
 
+#include <thread>
+
 class HD_Logger : public HD_Singleton<HD_Logger>
 {
 public:
-	
+	enum LogLevel : char
+	{
+		Invalid,
+		Log,
+		Warning,
+		Error,
+	};
 
 private:
+	HD_Logger();
+	~HD_Logger();
 
+	void LogThread();
+
+	std::thread myLogThread;
 };
