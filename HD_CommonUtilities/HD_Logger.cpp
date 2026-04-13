c002cbdd1cf59014624d359fcf3b2a43a2c1c42a
Hjalmar Danielson <hjalmar.danielson@gmail.com>
2026-04-13 23:17:17 +0200

    Adding HD_Logger. Tests remain to be written.

--- HD_CommonUtilities/HD_Logger.cpp
+++ HD_CommonUtilities/HD_Logger.cpp
@@ -1,12 +1,140 @@
 #include "stdafx.h"
-#include"HD_Logger.h"
+#include "HD_Logger.h"
+
+#define WIN32_LEAN_AND_MEAN
+
+#define NOGDICAPMASKS
+#define NOVIRTUALKEYCODES
+#define NOWINMESSAGES
+#define NOWINSTYLES
+#define NOSYSMETRICS
+#define NOMENUS
+#define NOICONS
+#define NOKEYSTATES
+#define NOSYSCOMMANDS
+#define NORASTEROPS
+#define NOSHOWWINDOW
+#define OEMRESOURCE
+#define NOATOM
+#define NOCLIPBOARD
+#define NOCOLOR
+#define NOCTLMGR
+#define NODRAWTEXT
+#define NOGDI
+#define NOKERNEL
+//#define NOUSER
+//#define NONLS - Required for CP_ACP and WideCharToMultiByte
+#define NOMB
+#define NOMEMMGR
+#define NOMETAFILE
+#define NOMINMAX
+#define NOMSG
+#define NOOPENFILE
+#define NOSCROLL
+#define NOSERVICE
+#define NOSOUND
+#define NOTEXTMETRIC
+#define NOWH
+#define NOWINOFFSETS
+#define NOCOMM
+#define NOKANJI
+#define NOHELP
+#define NOPROFILER
+#define NODEFERWINDOWPOS
+#define NOMCX
+#include <Windows.h>
+
+#include <iostream>
 
 HD_Logger::HD_Logger()
+	: myIsRunning(true)
+	, myStdErrHandle(GetStdHandle(STD_ERROR_HANDLE))
 {
 	myLogThread = std::thread(&HD_Logger::LogThread, this);
 }
 
 HD_Logger::~HD_Logger()
 {
+	myIsRunning = false;
+	myLogEntriesConditionVariable.notify_all();
 	myLogThread.join();
+}
+
+void HD_Logger::Log(const char* aLogMessage, LogLevel aLogLevel)
+{
+	LogEntry logEntry;
+	logEntry.myLogMessage = aLogMessage;
+	logEntry.myLogLevel = aLogLevel;
+
+	{
+		std::scoped_lock lock(myLogEntriesMutex);
+		myLogEntryQueue.PushBack(HD_Move(logEntry));
+	}
+
+	myLogEntriesConditionVariable.notify_one();
+}
+
+void HD_Logger::LogThread()
+{
+	HD_GrowingArray<LogEntry> localLogEntryQueue;
+
+	while (myIsRunning)
+	{
+		{
+			std::unique_lock lock(myLogEntriesMutex);
+
+			myLogEntriesConditionVariable.wait(lock, [this]
+			{
+				return !myIsRunning || !myLogEntryQueue.GetIsEmpty();
+			});
+
+			if (!myIsRunning && myLogEntryQueue.GetIsEmpty())
+			{
+				break;
+			}
+
+			HD_Swap(localLogEntryQueue, myLogEntryQueue);
+		}
+
+		for (int i = 0; i < localLogEntryQueue.GetSize(); i++)
+		{
+			PrintLogEntry(localLogEntryQueue[i]);
+		}
+
+		localLogEntryQueue.RemoveAll();
+	}
+}
+
+void HD_Logger::PrintLogEntry(const LogEntry& aLogEntry) const
+{
+	SetConsoleTextAttribute(myStdErrHandle, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+
+	switch (aLogEntry.myLogLevel)
+	{
+	case LogLevel_Invalid:
+		break;
+	case LogLevel_Log:
+	{
+		SetConsoleTextAttribute(myStdErrHandle, BACKGROUND_RED | BACKGROUND_GREEN | BACKGROUND_BLUE);
+		std::cerr << "[  ERROR  ]";
+		SetConsoleTextAttribute(myStdErrHandle, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY);
+	}
+	case LogLevel_Warning:
+	{
+		SetConsoleTextAttribute(myStdErrHandle, BACKGROUND_RED | BACKGROUND_GREEN);
+		std::cerr << "[ WARNING ]";
+		SetConsoleTextAttribute(myStdErrHandle, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+	}
+	case LogLevel_Error:
+	{
+		SetConsoleTextAttribute(myStdErrHandle, BACKGROUND_RED);
+		std::cerr << "[   LOG   ]";
+		SetConsoleTextAttribute(myStdErrHandle, FOREGROUND_RED | FOREGROUND_INTENSITY);
+	}
+	}
+
+	std::cerr << aLogEntry.myLogMessage.GetBuffer() << '\n';
+	std::cerr.flush();
+
+	SetConsoleTextAttribute(myStdErrHandle, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
 }
--- HD_CommonUtilities/HD_Logger.h
+++ HD_CommonUtilities/HD_Logger.h
@@ -1,25 +1,58 @@
 #pragma once
 
+#include "HD_GrowingArray.h"
 #include "HD_Singleton.h"
+#include "HD_String.h"
 
+#include <condition_variable>
+#include <mutex>
 #include <thread>
+
+#define LOG_MESSAGE(aLogMessage) HD_Logger::GetInstance().Log(aLogMessage, HD_Logger::LogLevel_Log);
+#define LOG_WARNING(aLogMessage) HD_Logger::GetInstance().Log(aLogMessage, HD_Logger::LogLevel_Warning);
+#define LOG_ERROR(aLogMessage) HD_Logger::GetInstance().Log(aLogMessage, HD_Logger::LogLevel_Error);
 
 class HD_Logger : public HD_Singleton<HD_Logger>
 {
 public:
 	enum LogLevel : char
 	{
-		Invalid,
-		Log,
-		Warning,
-		Error,
+		LogLevel_Invalid,
+		LogLevel_Log,
+		LogLevel_Warning,
+		LogLevel_Error,
 	};
 
+	void Log(const char* aLogMessage, LogLevel aLogLevel);
+
 private:
+	struct LogEntry
+	{
+		LogEntry& operator=(LogEntry&& aLogEntry)
+		{
+			myLogMessage = HD_Move(aLogEntry.myLogMessage);
+			myLogLevel = aLogEntry.myLogLevel;
+
+			return *this;
+		}
+
+		HD_String myLogMessage;
+		LogLevel myLogLevel;
+	};
+
 	HD_Logger();
 	~HD_Logger();
 
 	void LogThread();
 
+	void PrintLogEntry(const LogEntry& aLogEntry) const;
+
 	std::thread myLogThread;
+	bool myIsRunning;
+
+	HD_GrowingArray<LogEntry> myLogEntryQueue;
+	std::mutex myLogEntriesMutex;
+	std::condition_variable myLogEntriesConditionVariable;
+
+	void* myStdErrHandle;
 };
