b33c0832bf7ad43252c3bba9d67c6226105a2555
Hjalmar Danielsson <hjalmar.danielsson@learnet.se>
2026-06-24 16:55:55 +0200

    Adding HD_Remap and HD_RemapClamped

--- Source/HD_CommonUtilities/Math/HD_Math.h
+++ Source/HD_CommonUtilities/Math/HD_Math.h
@@ -30,28 +30,141 @@ template<typename T> inline T HD_ArcCos(T aAngleInRadians);
 template<typename T> inline T HD_ArcTan(T aAngleInRadians);
 template<typename T> inline T HD_ArcTan2(T aAngleYInRadians, T aAngleXInRadians);
 
-template<> inline float HD_Sqrt(float aValue) { return sqrtf(aValue); }
-template<> inline float HD_Pow(float aBase, float aExponent) { return powf(aBase, aExponent); }
+template<typename T> inline T HD_Remap(T aValue, T aOldMin, T aOldMax, T aNewMin, T aNewMax);
+template<typename T> inline T HD_RemapClamped(T aValue, T aOldMin, T aOldMax, T aNewMin, T aNewMax);
 
-template<> inline double HD_Sqrt(double aValue) { return sqrt(aValue); }
-template<> inline double HD_Pow(double aBase, double aExponent) { return pow(aBase, aExponent); }
+template<>
+inline float HD_Sqrt(float aValue)
+{
+	return sqrtf(aValue);
+}
 
-template<> inline int HD_Pow(int aBase, int aExponent) { return static_cast<int>(powf(static_cast<float>(aBase), static_cast<float>(aExponent))); }
+template<>
+inline double HD_Sqrt(double aValue)
+{
+	return sqrt(aValue);
+}
 
-template<> inline float HD_Sin(float aAngleInRadians) { return sinf(aAngleInRadians); }
-template<> inline float HD_Cos(float aAngleInRadians) { return cosf(aAngleInRadians); }
-template<> inline float HD_Tan(float aAngleInRadians) { return tanf(aAngleInRadians); }
+template<>
+inline float HD_Pow(float aBase, float aExponent)
+{
+	return powf(aBase, aExponent);
+}
 
-template<> inline double HD_Sin(double aAngleInRadians) { return sin(aAngleInRadians); }
-template<> inline double HD_Cos(double aAngleInRadians) { return cos(aAngleInRadians); }
-template<> inline double HD_Tan(double aAngleInRadians) { return tan(aAngleInRadians); }
+template<>
+inline double HD_Pow(double aBase, double aExponent)
+{
+	return pow(aBase, aExponent);
+}
 
-template<> inline float HD_ArcSin(float aAngleInRadians) { return asinf(aAngleInRadians); }
-template<> inline float HD_ArcCos(float aAngleInRadians) { return acosf(aAngleInRadians); }
-template<> inline float HD_ArcTan(float aAngleInRadians) { return atanf(aAngleInRadians); }
-template<> inline float HD_ArcTan2(float aAngleYInRadians, float aAngleXInRadians) { return atan2f(aAngleYInRadians, aAngleXInRadians); }
+template<>
+inline int HD_Pow(int aBase, int aExponent)
+{
+	return static_cast<int>(powf(static_cast<float>(aBase), static_cast<float>(aExponent)));
+}
 
-template<> inline double HD_ArcSin(double aAngleInRadians) { return asin(aAngleInRadians); }
-template<> inline double HD_ArcCos(double aAngleInRadians) { return acos(aAngleInRadians); }
-template<> inline double HD_ArcTan(double aAngleInRadians) { return atan(aAngleInRadians); }
-template<> inline double HD_ArcTan2(double aAngleYInRadians, double aAngleXInRadians) { return atan2(aAngleYInRadians, aAngleXInRadians); }
+template<>
+inline float HD_Sin(float aAngleInRadians)
+{
+	return sinf(aAngleInRadians);
+}
+
+template<>
+inline double HD_Sin(double aAngleInRadians)
+{
+	return sin(aAngleInRadians);
+}
+
+template<>
+inline float HD_Cos(float aAngleInRadians)
+{
+	return cosf(aAngleInRadians);
+}
+
+template<>
+inline double HD_Cos(double aAngleInRadians)
+{
+	return cos(aAngleInRadians);
+}
+
+template<>
+inline float HD_Tan(float aAngleInRadians)
+{
+	return tanf(aAngleInRadians);
+}
+
+template<>
+inline double HD_Tan(double aAngleInRadians)
+{
+	return tan(aAngleInRadians);
+}
+
+template<>
+inline float HD_ArcSin(float aAngleInRadians)
+{
+	return asinf(aAngleInRadians);
+}
+
+template<>
+inline double HD_ArcSin(double aAngleInRadians)
+{
+	return asin(aAngleInRadians);
+}
+
+template<>
+inline float HD_ArcCos(float aAngleInRadians)
+{
+	return acosf(aAngleInRadians);
+}
+
+template<>
+inline double HD_ArcCos(double aAngleInRadians)
+{
+	return acos(aAngleInRadians);
+}
+
+template<>
+inline float HD_ArcTan(float aAngleInRadians)
+{
+	return atanf(aAngleInRadians);
+}
+
+template<>
+inline double HD_ArcTan(double aAngleInRadians)
+{
+	return atan(aAngleInRadians);
+}
+
+template<>
+inline float HD_ArcTan2(float aAngleYInRadians, float aAngleXInRadians)
+{
+	return atan2f(aAngleYInRadians, aAngleXInRadians);
+}
+
+template<>
+inline double HD_ArcTan2(double aAngleYInRadians, double aAngleXInRadians)
+{
+	return atan2(aAngleYInRadians, aAngleXInRadians);
+}
+
+template<typename T>
+inline T HD_Remap(T aValue, T aOldMin, T aOldMax, T aNewMin, T aNewMax)
+{
+	return (((aValue - aOldMin) / (aOldMax - aOldMin)) * (aNewMax - aNewMin)) + aNewMin;
+}
+
+template<typename T>
+inline T HD_RemapClamped(T aValue, T aOldMin, T aOldMax, T aNewMin, T aNewMax)
+{
+	if (aValue < aOldMin)
+	{
+		return aNewMin;
+	}
+
+	if (aValue > aOldMax)
+	{
+		return aNewMax;
+	}
+
+	return HD_Remap(aValue, aOldMin, aOldMax, aNewMin, aNewMax);
+}
--- Source/Tests/Math/HD_Math_Tests.cpp
+++ Source/Tests/Math/HD_Math_Tests.cpp
@@ -3,6 +3,8 @@
 
 #include "HD_Math.h"
 
+#include "TestUtils.h"
+
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace HD_CommonUtilities
@@ -10,32 +12,13 @@ namespace HD_CommonUtilities
 	TEST_CLASS(Math)
 	{
 	public:
-		TEST_METHOD(SquareRoot)
-		{
-			Assert::AreEqual(HD_Sqrt(4.f), 2.f);
-		}
-
-		TEST_METHOD(AbsoluteValue)
-		{
-			Assert::AreEqual(HD_Abs(2), 2);
-			Assert::AreEqual(HD_Abs(-2), 2);
-			Assert::AreEqual(HD_Abs(2.f), 2.f);
-			Assert::AreEqual(HD_Abs(-2.f), 2.f);
-		}
-
-		TEST_METHOD(Power)
-		{
-			Assert::AreEqual(HD_Pow(2, 2), 4);
-			Assert::AreEqual(HD_Pow(3.f, 3.f), 27.f);
-		}
-
 		TEST_METHOD(Max)
 		{
 			Assert::AreEqual(HD_Max(0, 0), 0);
 			Assert::AreEqual(HD_Max(1, 2), 2);
 			Assert::AreEqual(HD_Max(2, 1), 2);
-			Assert::AreEqual(HD_Max(-1.f, 1.f), 1.f);
-			Assert::AreEqual(HD_Max(1.f, -1.f), 1.f);
+			Assert::AreEqual(HD_Max(-1.f, 1.f), 1.f, FLOAT_EQUAL_TOLERANCE);
+			Assert::AreEqual(HD_Max(1.f, -1.f), 1.f, FLOAT_EQUAL_TOLERANCE);
 		}
 
 		TEST_METHOD(Min)
@@ -43,8 +26,41 @@ namespace HD_CommonUtilities
 			Assert::AreEqual(HD_Min(0, 0), 0);
 			Assert::AreEqual(HD_Min(1, 2), 1);
 			Assert::AreEqual(HD_Min(2, 1), 1);
-			Assert::AreEqual(HD_Min(-1.f, 1.f), -1.f);
-			Assert::AreEqual(HD_Min(1.f, -1.f), -1.f);
+			Assert::AreEqual(HD_Min(-1.f, 1.f), -1.f, FLOAT_EQUAL_TOLERANCE);
+			Assert::AreEqual(HD_Min(1.f, -1.f), -1.f, FLOAT_EQUAL_TOLERANCE);
+		}
+
+		TEST_METHOD(Abs)
+		{
+			Assert::AreEqual(HD_Abs(2), 2);
+			Assert::AreEqual(HD_Abs(-2), 2);
+			Assert::AreEqual(HD_Abs(2.f), 2.f, FLOAT_EQUAL_TOLERANCE);
+			Assert::AreEqual(HD_Abs(-2.f), 2.f, FLOAT_EQUAL_TOLERANCE);
+		}
+
+		TEST_METHOD(Sqrt)
+		{
+			Assert::AreEqual(HD_Sqrt(4.f), 2.f, FLOAT_EQUAL_TOLERANCE);
+		}
+
+		TEST_METHOD(Pow)
+		{
+			Assert::AreEqual(HD_Pow(2, 2), 4);
+			Assert::AreEqual(HD_Pow(3.f, 3.f), 27.f, FLOAT_EQUAL_TOLERANCE);
+		}
+
+		TEST_METHOD(Remap)
+		{
+			Assert::AreEqual(HD_Remap(5.f, 0.f, 10.f, 100.f, 200.f), 150.f, FLOAT_EQUAL_TOLERANCE);
+			Assert::AreEqual(HD_Remap(-5.f, 0.f, 10.f, 100.f, 200.f), 50.f, FLOAT_EQUAL_TOLERANCE);
+			Assert::AreEqual(HD_Remap(15.f, 0.f, 10.f, 100.f, 200.f), 250.f, FLOAT_EQUAL_TOLERANCE);
+		}
+
+		TEST_METHOD(RemapClamped)
+		{
+			Assert::AreEqual(HD_RemapClamped(5.f, 0.f, 10.f, 100.f, 200.f), 150.f, FLOAT_EQUAL_TOLERANCE);
+			Assert::AreEqual(HD_RemapClamped(-5.f, 0.f, 10.f, 100.f, 200.f), 100.f, FLOAT_EQUAL_TOLERANCE);
+			Assert::AreEqual(HD_RemapClamped(15.f, 0.f, 10.f, 100.f, 200.f), 200.f, FLOAT_EQUAL_TOLERANCE);
 		}
 	};
 }
