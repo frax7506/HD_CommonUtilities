e3e909ac3b27cbba10fda78b0f0f39bdd36594f2
Hjalmar Danielsson <hjalmar.danielsson@learnet.se>
2026-07-06 15:00:38 +0200

    Putting the utility colors in a cpp to avoid linker errors. Could be inlined instead but I think this is cleaner.

---
+++ Source/HD_CommonUtilities/Misc/HD_Color.cpp
@@ -0,0 +1,10 @@
+#include "HD_Color.h"
+
+const HD_Color HD_Color::Black = { 0.f, 0.f, 0.f, 1.f };
+const HD_Color HD_Color::White = { 1.f, 1.f, 1.f, 1.f };
+const HD_Color HD_Color::Red = { 1.f, 0.f, 0.f, 1.f };
+const HD_Color HD_Color::Green = { 0.f, 1.f, 0.f, 1.f };
+const HD_Color HD_Color::Blue = { 0.f, 0.f, 1.f, 1.f };
+const HD_Color HD_Color::Yellow = { 1.f, 1.f, 0.f, 1.f };
+const HD_Color HD_Color::Purple = { 1.f, 0.f, 1.f, 1.f };
+const HD_Color HD_Color::Teal = { 0.f, 1.f, 1.f, 1.f };
--- Source/HD_CommonUtilities/Misc/HD_Color.h
+++ Source/HD_CommonUtilities/Misc/HD_Color.h
@@ -16,12 +16,3 @@ struct HD_Color
 	static const HD_Color Purple;
 	static const HD_Color Teal;
 };
-
-const HD_Color HD_Color::Black = { 0.f, 0.f, 0.f, 1.f };
-const HD_Color HD_Color::White = { 1.f, 1.f, 1.f, 1.f };
-const HD_Color HD_Color::Red = { 1.f, 0.f, 0.f, 1.f };
-const HD_Color HD_Color::Green = { 0.f, 1.f, 0.f, 1.f };
-const HD_Color HD_Color::Blue = { 0.f, 0.f, 1.f, 1.f };
-const HD_Color HD_Color::Yellow = { 1.f, 1.f, 0.f, 1.f };
-const HD_Color HD_Color::Purple = { 1.f, 0.f, 1.f, 1.f };
-const HD_Color HD_Color::Teal = { 0.f, 1.f, 1.f, 1.f };
