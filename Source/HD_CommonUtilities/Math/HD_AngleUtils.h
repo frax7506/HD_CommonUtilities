959a805fa7ac12a29b2157cbbf9e2bd1f223870c
Hjalmar Danielsson <hjalmar.danielsson@learnet.se>
2026-08-24 14:47:28 +0200

    Just moving the angle utils implementations to a separate cpp

--- Source/HD_CommonUtilities/Math/HD_AngleUtils.h
+++ Source/HD_CommonUtilities/Math/HD_AngleUtils.h
@@ -6,20 +6,6 @@
 
 namespace HD_AngleUtils
 {
-	f32 SignedAngleBetweenVectors(const HD_Vector2_f32& aVector1, const HD_Vector2_f32& aVector2)
-	{
-		// Returns the smallest angle that rotates aVector2 to aVector1.
-		// Clockwise rotation returns a negative angle, counter clockwise rotation returns a positive angle.
-		// Source: https://wumbo.net/formulas/angle-between-two-vectors-2d/
-
-		const f32 angle = HD_ArcTan2(aVector2.myX * aVector1.myY - aVector2.myY * aVector1.myX, aVector2.myX * aVector1.myX + aVector2.myY * aVector1.myY);
-		return angle;
-	}
-
-	f32 AngleBetweenVectors(const HD_Vector2_f32& aVector1, const HD_Vector2_f32& aVector2)
-	{
-		const f32 signedAngle = SignedAngleBetweenVectors(aVector1, aVector2);
-		const f32 unsignedAngle = signedAngle * HD_Sign(signedAngle);
-		return unsignedAngle;
-	}
+	f32 SignedAngleBetweenVectors(const HD_Vector2_f32& aVector1, const HD_Vector2_f32& aVector2);
+	f32 AngleBetweenVectors(const HD_Vector2_f32& aVector1, const HD_Vector2_f32& aVector2);
 }
